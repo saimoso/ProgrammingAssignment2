f87919b03176bbd4b59425698c6d9fcce3217616
saimoso <dotping@dotpingMBA.local>
2015-07-25 17:45:11 +0400

    Completed the assignment
    Edited the R file, namely the two functions, to meet the requirements.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,52 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
-
+# Return a list of setter/getter functions 
+# for a matrix and its inverse:
+## 1. set matrix
+## 2. get matrix
+## 3. set inverse
+## 4. get inverse
 makeCacheMatrix <- function(x = matrix()) {
-
+      inv <<- NULL
+      
+      set <- function(y){
+            x <<- y
+            inv <<- NULL
+      }
+      
+      get <- function() x
+      
+      setinv <- function(inverse) inv <<- inverse
+      
+      getinv <- function() inv
+      
+      list(set = set, 
+           get = get, 
+           setinv = setinv,
+           getinv = getinv)
 }
 
 
-## Write a short comment describing this function
-
+## Return the inverse, IF a) it has already been calculated 
+##    AND b) the matrix hasn't changed.
+## Otherwise, calculate the inverse again.
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+      # try to fetch the inverse in cache
+      inv <- x$getinv()
+      
+      # check if the inv was in cache
+      if(!is.null(inv)){
+            message("Retrieving cached inverse...")
+            # ... and return it
+            return(inv)
+      }
+      
+      # otherwise, calculate the inverse
+      mat <- x$get()
+      inv <- solve(mat, ...)
+      # ... set it
+      x$setinv(inv)
+      ## Return a matrix that is the inverse of 'x'
+      inv
 }
